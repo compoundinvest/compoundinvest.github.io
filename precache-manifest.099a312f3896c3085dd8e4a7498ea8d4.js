c1f8125e78f0df334d671065cbcef344bbf6a36b
Robert <zakiev@me.com>
2022-12-01 23:44:37 +0100

    Updates

---
+++ precache-manifest.099a312f3896c3085dd8e4a7498ea8d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d11509221d3e820a8e36",
+    "url": "/static/css/main.1d0d7f3a.chunk.css"
+  },
+  {
+    "revision": "d11509221d3e820a8e36",
+    "url": "/static/js/main.4a15a91a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6f1aa96221b5e99c4e50",
+    "url": "/static/js/2.48134459.chunk.js"
+  },
+  {
+    "revision": "40c90653b7395b9a78b543d7ae47f887",
+    "url": "/index.html"
+  }
+];
